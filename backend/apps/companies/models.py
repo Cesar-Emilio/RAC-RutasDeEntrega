275af6e69fa7901e85f76fa9920772508e06c654
astVVG <20233tn158@utez.edu.mx>
2026-03-18 12:36:49 -0600

    [ADD]: App - Companies

--- backend/apps/companies/models.py
+++ backend/apps/companies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
-# Create your models here.
+class Company(models.Model):
+    # validaciones para el RFC
+    rfc_validator = RegexValidator(
+        regex=r'^[A-ZÑ&]{3,4}\d{6}[A-Z0-9]{3}$',
+        message='RFC inválido'
+    )
+
+    name = models.CharField(max_length=255)
+    rfc = models.CharField(max_length=13, unique=True, validators=[rfc_validator])
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.rfc})"
+    
+    # class Meta:
+    #     ordering = ['-created_at']
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework.routers import DefaultRouter
+from django.conf import settings
+from django.conf.urls.static import static
+
+# viewSets de las apps
+from apps.companies.views import CompanyViewSet
+
+router = DefaultRouter()
+router.register(r'companies', CompanyViewSet, basename='company')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
