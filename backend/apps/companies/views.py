93c38808d152a7ba88ccb9483c8cc6af37a48641
astVVG <20233tn158@utez.edu.mx>
2026-03-18 12:36:49 -0600

    [ADD]: App - Companies

--- backend/apps/companies/views.py
+++ backend/apps/companies/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Company
+from .serializers import CompanySerializer
 
-# Create your views here.
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.filter(active=True)
+    serializer_class = CompanySerializer
