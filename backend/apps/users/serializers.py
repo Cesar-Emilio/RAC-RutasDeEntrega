c85b7a7030c0697e36f267b0236a511991183d91
astVVG <20233tn158@utez.edu.mx>
2026-03-19 00:39:42 -0600

    [UPDATED]: Users

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -3,7 +3,26 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+# serializer para registro de uusarios
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'name','password', 'role', 'company')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+    # Validacion para la contraseña
+    def validate(self, data):
+        if not data.get('password'):
+            raise serializers.ValidationError("La contraseña es obligatoria")
+        return data
+    
+# serializer para lectura de usuarios
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ()
+        fields = ('id', 'email', 'name', 'role', 'company', 'active')
