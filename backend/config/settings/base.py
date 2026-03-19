f50dc8ee3dcc65d0541916ebec7541030dcc1625
astVVG <20233tn158@utez.edu.mx>
2026-03-19 00:39:42 -0600

    [UPDATED]: Users

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'apps.companies',
     'apps.deliveries',
     'apps.warehouses',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+AUTH_USER_MODEL = 'users.User'
