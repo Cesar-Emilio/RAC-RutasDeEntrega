a0689ec9ed4a7a82eafd47c988c7e0118fe014c2
Sebastian Jimenez <20233tn097@utez.edu.mx>
2026-04-05 11:21:47 -0600

    feat(warehouses): Implementado CRUD de almacenes con validaciones

--- backend/apps/warehouses/views.py
+++ backend/apps/warehouses/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
+from .models import Warehouse
+from .serializers import WarehouseSerializer
 
-# Create your views here.
+
+class WarehouseViewSet(viewsets.ModelViewSet):
+    serializer_class = WarehouseSerializer
+
+    def get_queryset(self):
+        return Warehouse.objects.filter(
+            active=True,
+            company=self.request.user.company
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(company=self.request.user.company)
+
+    def destroy(self, request, *args, **kwargs):
+        warehouse = self.get_object()
+        warehouse.active = False
+        warehouse.save()
+        return Response(
+            {'detail': 'Almacén desactivado correctamente.'},
+            status=status.HTTP_200_OK
+        )
